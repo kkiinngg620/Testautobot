ba5cf5ad45d89d5aef30d5d087ffb60387031b6e
kkiinngg620 <133698348+kkiinngg620@users.noreply.github.com>
2023-07-09 10:28:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Channel Button Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/allmovises0')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Moviesbank_Corporation')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/mr_chaudhary620')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
 MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 
 # Custom Chats
